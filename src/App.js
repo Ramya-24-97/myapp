e1dde35ea39f1e77e33c6d2b4fb36f2725dd9976
Ramya <141641596+Ramya-24-97@users.noreply.github.com>
2023-08-26 22:53:00 +0530

    js linked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Art from "./pages/Art";
+import Faq from "./pages/Faq";
+import Video from "./pages/Video";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +10,8 @@ function App() {
   return (
     <div className="App">
      <Art />
+     <Faq />
+     <Video />
     </div>
   );
 }
