94591f4182eaef42e0d015ea8658d7378e6b4f6c
Ramya <141641596+Ramya-24-97@users.noreply.github.com>
2023-08-27 11:10:00 +0530

    cta added to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Art from "./pages/Art";
 import Faq from "./pages/Faq";
 import Video from "./pages/Video";
+import Contact from "./pages/Contact";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +13,7 @@ function App() {
      <Art />
      <Faq />
      <Video />
+     <Contact />
     </div>
   );
 }
